61ffd28f06b9a2f2daf67ff2b4876546f90a28d3
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-05-24 21:20:24 +0200

    Added info

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject hafni "1.0.0-SNAPSHOT"
+(defproject 
+  hafni "1.0.0-SNAPSHOT"
   :description "Hafni is a java swing wrapper for the clojure language focusing on how components change."
+  :url "http://github.com/odyssomay/Hafni"
+  :license {:name "zlib License"
+            :url "http://en.wikipedia.org/wiki/zlib_License"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.1.2"]
                  [clj-diff "1.0.0-SNAPSHOT"]]
