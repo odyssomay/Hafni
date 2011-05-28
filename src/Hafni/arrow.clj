8c70bd7db32507f8f80ef860e42597e7dc04ec9d
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-05-28 16:46:26 +0200

    added simple error handling

--- src/Hafni/arrow.clj
+++ src/Hafni/arrow.clj
@@ -1,4 +1,5 @@
-(ns Hafni.arrow)
+(ns Hafni.arrow
+  (:use clojure.tools.logging))
 
 (defn clone [x]
   [x x])
@@ -19,19 +20,43 @@
   (snd [this])
   (*** [this a])
   (&&& [this a])
-  (||| [this arr1 arr2]))
+  (||| [this arr1 arr2] [this arr1]))
 
 (defrecord Arrow [f]
   clojure.lang.IFn
   (invoke [this input] ((:f this) input))
   Arrow_p
-  (>>> [this dest] (assoc this :f #((:f dest) ((:f this) %))))
-  (<<< [this src] (>>> src this))
+  (>>> [this dest] 
+       (if (isa? (class this) (class dest))
+           (assoc this :f #((:f dest) ((:f this) %)))
+           (error ">>> was called, but dest isn't an arrow.")))
+
+  (<<< [this src]
+       (if (isa? (class this) (class src))
+           (>>> src this)
+           (error "<<< was called, but src isn't an arrow.")))
+
   (fst [this] (assoc this :f (fn [[x y]] [(this x) y])))
   (snd [this] (assoc this :f (fn [[x y]] [x (this y)])))
-  (*** [this a] (>>> (fst this) (snd a)))
-  (&&& [this a] (assoc this :f (fn [x] ((*** this a) [x x]))))
-  (||| [this arr1 arr2] (>>> (fst this) (iarr this #(if (first %) (arr1 %) (arr2 %))))))
+
+  (*** [this a]
+       (if (isa? (class this) (class a))
+           (>>> (fst this) (snd a))
+           (error "*** was called, but a isn't an arrow")))
+
+  (&&& [this a] 
+       (if (isa? (class this) (class a))
+           (assoc this :f (fn [x] ((*** this a) [x x])))
+           (error "&&& was called, but a isn't an arrow.")))
+
+  (||| [this arr1 arr2]
+       (if (isa? (class this) (class arr1))
+         (if (isa? (class this) (class arr2))
+           (>>> (fst this) (iarr this #(if (first %) (arr1 %) (arr2 %))))
+           (error "||| was called, but arr2 isn't an arrow."))
+         (error "||| was called, but arr1 isn't an arrow.")))
+
+  (||| [this arr1] (||| this arr1 (iarr this (fn [_] nil)))))
 
 (defn arr [f]
   (Arrow. f))
