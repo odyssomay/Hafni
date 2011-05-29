e4699ffb4720e5591fd8256aaaf44a706db54bb6
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-05-29 23:09:04 +0200

    replaced color macro with a function

--- src/Hafni/swing/utils.clj
+++ src/Hafni/swing/utils.clj
@@ -1,7 +1,7 @@
 (ns Hafni.swing.utils
   (:use clojure.tools.logging)
   (:import java.io.File
-           (java.awt Font GraphicsEnvironment)))
+           (java.awt Color Font GraphicsEnvironment)))
 
 (def *available-fonts*
   (vec (.getAvailableFontFamilyNames (GraphicsEnvironment/getLocalGraphicsEnvironment))))
@@ -24,13 +24,26 @@ between the arguments."
     (error "Font family doesn't exist - using default."))
   (Font. name Font/PLAIN size))
 
-(defmacro color
+(defn color
   "Returns an instance of java.awt.Color.
 The three argument version takes red, green, blue.
-The one argument version takes a symbol, which is any of:
-  black, blue, cyan, darkGray, gray, green, lightGray,
+The one argument version takes a string, which is any of:
+  black, blue, cyan, dark_gray, gray, green, light_gray,
   magenta, orange, pink, red, white, yellow."
   ([r g b]
-   `(java.awt.Color. ~r ~g ~b))
+   (Color. r g b))
   ([name]
-   `(load-string (str "java.awt.Color/" '~name))))
+   (case name
+     "black" Color/black
+     "blue" Color/blue
+     "cyan" Color/cyan
+     "dark_gray" Color/darkGray
+     "gray" Color/gray
+     "green" Color/green
+     "light_gray" Color/lightGray
+     "magenta" Color/magenta
+     "orange" Color/orange
+     "pink" Color/pink
+     "red" Color/red
+     "white" Color/white
+     "yellow" Color/yellow)))
