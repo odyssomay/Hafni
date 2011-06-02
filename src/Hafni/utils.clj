10dd55a1cb9fe7994852e59dec023cb403d4fbe3
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-06-02 15:46:41 +0200

    added coll changing functions

--- src/Hafni/utils.clj
+++ src/Hafni/utils.clj
@@ -5,6 +5,34 @@
     (map #(rmap f %) coll)
     (f coll)))
 
+(defn find-i
+  "Find the index of value in coll"
+  [value coll]
+  (let [limit (count coll)]
+    (loop [i 0
+           c coll]
+      (if (== i limit)
+        nil
+        (if (= (first c) value)
+            i
+            (recur (inc i) (rest c)))))))
+
+(defn replace-i
+  "change index n in coll to value"
+  [n value coll]
+  (concat (take n coll)
+          [value]
+          (drop (inc n) coll)))
+
+(defn change-i
+  "change index n in coll with f"
+  [n f coll]
+  (replace-i n (f (nth coll n)) coll))
+
+(defn drop-nth [n coll]
+  (concat (take n coll)
+          (rest (drop n coll))))
+
 (defn parse-options 
   "Creates a hash-map with the collection options.
 Keys are assumed to be keywords and keywords with no 
@@ -60,6 +88,3 @@ number of args and which always evaluates to x."
                       (((first x) in_arrs) (second x))))
               options)))
 
-(defn drop-nth [n coll]
-  (concat (take n coll)
-          (rest (drop n coll))))
