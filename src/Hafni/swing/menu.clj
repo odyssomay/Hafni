81d7abb499c8ef988a26f5dc88259062599feb2d
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-06-09 20:02:34 +0200

    fixed menu :content arrow. Added popup-menu

--- src/Hafni/swing/menu.clj
+++ src/Hafni/swing/menu.clj
@@ -1,13 +1,14 @@
 (ns Hafni.swing.menu
   (:use (Hafni utils) 
-        (Hafni.swing component))
+        (Hafni.swing component container view))
   (:import (javax.swing JMenu JMenuBar 
-                        JMenuItem JToolBar)))
+                        JMenuItem JToolBar SwingConstants)))
 
-(defn- add-content [cont x]
-  (dorun (map #(if (and (coll? %) (empty? %))
-                 (.addSeparator cont)
-                 (.add cont (component %))) x)))
+(defn- change-menu-content [m last_items new_items]
+  (change-container-content #(if (= %2 [])
+                                 (.insertSeparator m %1)
+                                 (.insert m (component %2) %1)) 
+                            #(.remove m %) last_items new_items))
 
 (defn menu 
   "Create a JMenu
@@ -16,10 +17,11 @@ Fields:
   :mnemonic - mnemonic key (see docstring for Hafni.swing.action/action) | Char"
   [title & options]
   (let [m (JMenu.)
+        last_items (atom [])
         arrs {:mnemonic #(.setMnemonic m (int %))
-              :content (partial add-content m)}]
+              :content (partial change-menu-content m last_items)}]
     (.setText m title)
-    (init-comp m arrs nil (parse-options options))))
+    (init-comp m arrs {} (parse-options options))))
 
 (defn menu-bar
   "Create a JMenuBar
@@ -27,8 +29,21 @@ Fields:
   :content - content of menu bar | [Component]"
   [& options]
   (let [m (JMenuBar.)
-        arrs {:content (partial add-content m)}]
-    (init-comp m arrs nil (parse-options options))))
+        last_items (atom [])
+        arrs {:content (partial change-menu-content m last_items)}]
+    (init-comp m arrs {} (parse-options options))))
+
+(defn popup-menu 
+  "DOES NOT create a JPopupMenu
+Displays menu at the mousepointers position.
+Parent can be nil, but will in that case not
+close when an item is choosed."
+  [parent menu]
+  (let [pmenu (.getPopupMenu (component menu))
+        mpos (mouse-position)
+        x (first mpos)
+        y (second mpos)]
+    (.show pmenu (component parent) x y)))
 
 (defn tool-bar
   "Create a JToolBar
@@ -38,8 +53,9 @@ Fields:
   :rollover - if true, shows a visual indicator under the cursor | Bool"
   [& options]
   (let [t (JToolBar.)
-        arrs {:content (partial add-content t)
+        last_items (atom [])
+        arrs {:content (partial change-menu-content t last_items)
               :floatable #(.setFloatable t %)
               :rollover #(.setRollover t %)}]
-    (init-comp t arrs nil (parse-options options))))
+    (init-comp t arrs {} (parse-options options))))
 
--- src/Hafni/test/swing/menu.clj
+++ src/Hafni/test/swing/menu.clj
@@ -1,8 +1,19 @@
 (ns Hafni.test.swing.menu
-  (:use (Hafni.swing action menu view)))
+  (:use (Hafni arrow utils) 
+        (Hafni.swing action component layout menu view)))
 
 (defn menu-toolbar-test []
-  (frame :menu_bar (menu-bar :content [(menu "menu 1" :content [(menu-item :name "item 1")
-                                                                (menu-item :name "item 2")])])
-         :size 300 300 :dont_exit_on_close))
+  (let [m (menu "menu 1" :content [(menu-item :text "item 1") []
+                                   (menu-item :text "item 2")])]
+    (frame :menu_bar (menu-bar :content [m])
+           :content (flow-layout 
+                      :content [(comp-and-events (button :text "add")
+                                                 :act (flow (output-arr m :content) >>> 
+                                                            (arr #(conj % (menu-item :text "new item!"))) >>> 
+                                                            (input-arr m :content)))
+                                (comp-and-events (button :text "remove")
+                                                 :act (flow (output-arr m :content) >>>
+                                                            (arr butlast) >>>
+                                                            (input-arr m :content)))])
+           :size 300 300 :dont_exit_on_close)))
 
